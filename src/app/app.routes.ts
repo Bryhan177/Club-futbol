7eab29c1017e7240c1e0dd8e34357aff3e57d7a3
Code-joseluis8 <joseluisquintho@gmail.com>
2025-04-07 20:24:39 -0500

    Cambios En Los Componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { EquiposComponent } from './components/equipos/equipos.component';
+import { LoginComponent } from './components/login/login.component';
+import { LugarJogoComponent } from './components/lugar-jogo/lugar-jogo.component';
+
+
 
 export const routes: Routes = [
-  {
+  { 
     path: 'home',
     component: HomeComponent,
+  },
+
+  {
+    path: 'equipos',
+    component: EquiposComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'lugar-jogo',
+    component: LugarJogoComponent,
   }
+  
+
 ];
